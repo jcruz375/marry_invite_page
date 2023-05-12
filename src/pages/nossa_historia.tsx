d50edd1498aeaad630816dce66b72b6cbe166cf3
joao.cruz@neopdv.com.br <joao.cruz@neopdv.com.br>
2023-05-12 02:09:44 -0300

    alteração pda página nossa historia

--- src/pages/nossa_historia.tsx
+++ src/pages/nossa_historia.tsx
@@ -11,6 +11,11 @@ import photo_9 from '../../public/assets/photo_9.png'
 import photo_10 from '../../public/assets/photo_10.png'
 import photo_11 from '../../public/assets/photo_11.png'
 import photo_12 from '../../public/assets/photo_12.png'
+import photo_13 from '../../public/assets/photo_13.png'
+import photo_14 from '../../public/assets/photo_14.png'
+import photo_15 from '../../public/assets/photo_15.png'
+import photo_16 from '../../public/assets/photo_16.png'
+import photo_17 from '../../public/assets/photo_17.png'
 
 export default function OurHistory() {
   return (
@@ -23,9 +28,13 @@ export default function OurHistory() {
           Como é bom recordar! Conheça os detalhes da nossa caminhada até esse grande sonho do nosso casamento.
         </p>
         <p className='font-serif text-xl text-center mt-16'>
-          Quando nos conhecemos, nem imaginávamos que um dia tudo isso acabaria como um final de conto de fadas, com a realização do nosso grande sonho: o casamento! O coração aperta, o dia está chegando, e começa a passar um filme de tudo o que passamos para chegar até aqui. E como é bom relembrar cada pedacinho da nossa história de amor.
+          Nos conhecemos a muito tempo (põe tempo nisso), mas por coisinhas do destino, não era para ser mesmo lá atras, não ficamos juntos....
+          Porém, como nada foge dos planos de Deus, por uma ironia, rede social, e uma piada qualquer, num dia a toa, cá estamos nós, mais maduros, com um bocado de filhos, mas com muito mais certeza do que queremos e em busca de um novo, ou talvez velho sonho.
+          E agora é praticamente impossível você conhecer um e não conhecer o outro! Sempre juntos, seja num samba ou em qualquer lugar, somos parceiros para TUDO que envolva o outro! Muita gente acha que é grude! Nós chamamos de AMOR e PARCERIA.
+          Já passamos por muitas coisas para estarmos juntos, muitos perrengues e apuros, mas muitas coisas boas também, e põe coisa boa nisso, muitas e muitas histórias....
+          E com toda a certeza, você fez e ou faz parte de algum desses momentos especiais para nós! E por isso, queremos você compartilhando mais esse momento com a gente!! Vamos nos casar e assim como nós estamos sempre juntos, queremos você juntinho com a gente nesse dia tão especial.
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-4 grid-rows-3 gap-8 mt-11 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-4 gap-8 mt-11 w-full">
           <div className="flex justify-center items-center">
             <Image
               className={`w-3/5 sm:w-full`}
@@ -54,63 +63,109 @@ export default function OurHistory() {
               alt="foto de claudia e eduardo"
             />
           </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_5}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_6}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_7}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_8}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_9}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_10}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_11}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              className={`w-3/5 sm:w-full`}
-              src={photo_12}
-              alt="foto de claudia e eduardo"
-            />
-          </div>
         </div>
+        <article className='mt-24 flex flex-col justify-start items-center'>
+          <p className='font-serif text-2xl text-center sm:text-left'>
+            Aos nossos queridos padrinhos e madrinhas
+          </p>
+          <p className='font-serif text-xl text-center mt-16'>
+            Estaríamos mentindo se disséssemos que foi uma escolha difícil.
+            Nunca pensamos em outras pessoas para estar ao nosso lado em um dia tão importante para nós. Vamos aproveitar juntos! Queremos que tirem muitas Fotos, bebam muito, nos ajudem a animar todos os convidados e que seja um dia inesquecível. Não só para nós, mas também para vocês!!
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-4 gap-8 mt-11 w-full">
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_5}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_6}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_7}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_8}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_9}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_10}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_11}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_12}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_13}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_14}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_15}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_16}
+                alt="foto dos padrinhos"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Image
+                className={`w-3/5 sm:w-full`}
+                src={photo_17}
+                alt="foto dos padrinhos"
+              />
+            </div>
+          </div>
+        </article>
       </section>
     </main>
   )
